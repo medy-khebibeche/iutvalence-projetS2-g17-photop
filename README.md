5c60d39262bae971a9f93f1813e35973327150ef
Medy <Medy@macbook-pro-de-medy.home>
2015-04-10 23:18:01 +0200

    Creation of the Eclipse project.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iutvalence-projetS2-g17-photop
+
 This is the repository of the java project of group 17 : Photop'
+
+Mathie BERTHOLET - Médy KHEBIBECHE - Jean-Baptiste MERCIER - Bastien PLANEILLE - Corentin Vallier
---
+++ src/fr/iutvalence/projet/s2/g17/photop/Main.java
@@ -0,0 +1,21 @@
+package fr.iutvalence.projet.s2.g17.photop;
+
+/**
+ * The main of the application
+ * @author Medy
+ *
+ */
+public class Main
+{
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
