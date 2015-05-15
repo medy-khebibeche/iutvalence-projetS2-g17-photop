a09f9e345b15bddc6fa9df80c68f70bffbebeab2
MacBoù <jean-baptiste.mercier@iut-valence.fr>
2015-05-15 10:18:41 +0200

    creation of the Decoration class

---
+++ src/fr/iutvalence/info/projet/s2/g17/photop/Decoration.java
@@ -0,0 +1,50 @@
+package fr.iutvalence.info.projet.s2.g17.photop;
+
+import java.awt.Color;
+import java.awt.color.*;
+/**
+ * Represents a decoration
+ * @author Jean-Baptiste
+ *
+ */
+public class Decoration
+{
+	/**
+	 * the color of the decoration
+	 */
+	private Color colorOfTheDecoration;
+	/**
+	 * the angle of the decoration
+	 */
+	private int angleOfTheDecoration;
+
+	/**
+	 * the constructor of a decoration
+	 * @param colorOfTheDecoration
+	 * @param angleofTheDecoration
+	 */
+	public Decoration(Color colorOfTheDecoration, int angleofTheDecoration)
+	{
+		Angle angleOfDecoration = new Angle(angleofTheDecoration);
+		this.angleOfTheDecoration = angleofTheDecoration;
+		this.colorOfTheDecoration = colorOfTheDecoration;
+	}
+	/**
+	 * Method who changes the color of a decoration
+	 * @param newColor
+	 */
+	public void changeColor(Color newColor)
+	{
+		this.colorOfTheDecoration = newColor;
+	}
+	/**
+	 * Method who rotates the decoration
+	 * @param newAngle
+	 */
+	public void rotationOfTheDecoration(int newAngle)
+	{
+		this.angleOfTheDecoration =  this.angleOfTheDecoration + newAngle;
+	}
+	
+	
+}
