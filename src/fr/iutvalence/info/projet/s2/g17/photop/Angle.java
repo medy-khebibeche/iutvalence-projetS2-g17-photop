6a502efa5785aedb76e8bed799906d0118e616dd
MacBoù <jean-baptiste.mercier@iut-valence.fr>
2015-05-15 10:18:05 +0200

    creation of the Angle class

---
+++ src/fr/iutvalence/info/projet/s2/g17/photop/Angle.java
@@ -0,0 +1,26 @@
+package fr.iutvalence.info.projet.s2.g17.photop;
+
+/**
+ * Represents an angle
+ * An angle has a value
+ * @author Jean-Baptiste
+ *
+ */
+public class Angle
+{
+	/**
+	 * The value of the angle
+	 */
+	private int angleValue;
+	
+	/**
+	 * Creates an angle
+	 * @param angleValue
+	 */
+	public Angle(int angleValue)
+	{
+		this.angleValue = angleValue;
+	}
+	
+
+}
