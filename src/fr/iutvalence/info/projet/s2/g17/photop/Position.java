ab611315c50522ecdc1c82734c283a08c8c12221
MacBoù <jean-baptiste.mercier@iut-valence.fr>
2015-05-15 10:19:08 +0200

    creation of the Position class

---
+++ src/fr/iutvalence/info/projet/s2/g17/photop/Position.java
@@ -0,0 +1,25 @@
+package fr.iutvalence.info.projet.s2.g17.photop;
+
+/**
+ * Represents a position of an object
+ * @author Jean-Baptiste
+ *
+ */
+public class Position
+{
+	/**
+	 * the abscissa
+	 */
+	private int xPosition;
+	/**
+	 * the ordinate
+	 */
+	private int yPosition;
+	
+	public Position(int xPosition, int yPosition)
+	{
+		this.xPosition =  xPosition;
+		this.yPosition = yPosition;
+	}
+
+}
