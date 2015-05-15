02282b04850f480241b48885e0e500e0f7b5e852
Medy <Medy@macbook-pro-de-medy.home>
2015-05-15 11:02:33 +0200

    Médy:
    +Creation of the Text Class (heritage of Decoration)
    	+attribute : font, text
    	+constructor Text(superclass of Decoration)
    	+method setFont(font)
    
    ?Shall we create a method to change the tex typed by the user ?

---
+++ src/fr/iutvalence/info/projet/s2/g17/photop/Text.java
@@ -0,0 +1,40 @@
+package fr.iutvalence.info.projet.s2.g17.photop;
+import java.awt.Color;
+/**
+ * 
+ * @author Medy
+ *
+ */
+public class Text extends Decoration
+{
+	/**
+	 * The font of the text
+	 */
+	private Font font;
+	/**
+	 * The text typed by the user
+	 */
+	private String text;
+	
+	/**
+	 * The constructor of the class Text, which is an heritage of Decoration
+	 * @param colorOfTheDecoration
+	 * @param angleofTheDecoration
+	 * @param positionOfTheDecoration
+	 * @param dimensionOfTheDecoration
+	 */
+	public Text(Color colorOfTheDecoration, int angleofTheDecoration, Position positionOfTheDecoration, Dimension dimensionOfTheDecoration)
+	{
+		super(colorOfTheDecoration, angleofTheDecoration, positionOfTheDecoration, dimensionOfTheDecoration);
+	}
+	/**
+	 * Allow to change and set a new font
+	 * @param font
+	 */
+	public void setFont(Font font)
+	{
+		this.font = font;
+	}
+	
+	//Doit-on faire une autre méthode pour le texte ??? - Médy
+}
