ab7a96f1696a90390be59267e947824d1fe8e750
AloneStone <bastien26700@yahoo.fr>
2015-06-08 15:51:40 +0200

    Test JUnit photop (beginning)

---
+++ src/fr/iutvalence/info/projet/s2/g17/photop/test/PositionTest.java
@@ -0,0 +1,55 @@
+package fr.iutvalence.info.projet.s2.g17.photop.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class Position
+ * @author Bastien
+ *
+ */
+
+public class PositionTest {
+
+	@Test
+	public void testCreationPosition() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+	@Test
+	public void testGetPosX() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+	@Test
+	public void testSetPosX() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+	@Test
+	public void testGetPosY() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+	@Test
+	public void testSetPosY() 
+	{
+		
+		fail("Not yet implemented");
+		
+	}
+
+}
